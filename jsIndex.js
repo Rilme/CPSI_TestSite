6f96735ea63b519de4ba977fb8060c49b4e8da38
Rilme <148802621+Rilme@users.noreply.github.com>
2024-02-22 23:57:45 -0600

    Update jsIndex.js

--- jsIndex.js
+++ jsIndex.js
@@ -15,11 +15,11 @@ fileInput.onchange = ({ target }) => {
       let splitName = fileName.split(".");
       fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
     }
-    uploadFile(fileName);
+    uploadFile(file, fileName); // Pass file and fileName to uploadFile function
   }
 };
 
-function uploadFile(name) {
+function uploadFile(file, name) {
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "upload.php");
   xhr.upload.addEventListener("progress", ({ loaded, total }) => {
@@ -59,16 +59,24 @@ function uploadFile(name) {
       uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
     }
   });
-  let data = new FormData(form);
-  xhr.send(data);
+
+  // Prepare the data to be sent to Power Automate
+  let formData = new FormData();
+  formData.append("file", file);
+  formData.append("name", name);
+
+  // Make an HTTP POST request to send data to Power Automate
+  let powerAutomateEndpoint = "https://prod-92.westus.logic.azure.com:443/workflows/b8aed472476d4e30a762027d45f29a3f/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=RvJN_krp58oEEFEz6KX-ye_0Yu-9v3hGlbN9JMQuMC4"; // Replace with your Power Automate endpoint
+  xhr.open("POST", powerAutomateEndpoint);
+  xhr.send(formData);
 }
 
 function confirmUpload() {
   if (
     confirm("Are you sure you want to submit the information for extraction?")
   ) {
-    // uploadFile();
-    window.location.href = "loadin.html"; // Redirect to Validation.html
+    fileInput.click(); // Trigger file input change event
+    // window.location.href = "loadin.html"; // Redirect to another page
   } else {
     // User clicked Cancel, do nothing
   }
