ea56c0f4b541c6a6fc42a6c0c11d25f112bd5b75
Rilme <148802621+Rilme@users.noreply.github.com>
2024-02-23 00:05:20 -0600

    Update jsIndex.js

--- jsIndex.js
+++ jsIndex.js
@@ -67,7 +67,7 @@ function uploadFile(file, name) {
 
   // Make an HTTP POST request to send data to Power Automate
   let powerAutomateEndpoint = "https://prod-92.westus.logic.azure.com:443/workflows/b8aed472476d4e30a762027d45f29a3f/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=RvJN_krp58oEEFEz6KX-ye_0Yu-9v3hGlbN9JMQuMC4"; // Replace with your Power Automate endpoint
-  xhr.open("fetch", powerAutomateEndpoint);
+  xhr.open("Fetch", powerAutomateEndpoint);
   xhr.send(formData);
 }
 
