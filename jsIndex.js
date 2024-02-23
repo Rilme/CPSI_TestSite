e39f858c72bc0d5f1aa74e551d9e19d0d5446438
Rilme <148802621+Rilme@users.noreply.github.com>
2024-02-23 08:06:25 -0600

    Update jsIndex.js

--- jsIndex.js
+++ jsIndex.js
@@ -1,27 +1,25 @@
-const form = document.querySelector("form"),
-  fileInput = document.querySelector(".file-input"),
-  progressArea = document.querySelector(".progress-area"),
-  uploadedArea = document.querySelector(".uploaded-area");
+const form = document.getElementById("uploadForm");
+const fileInput = document.getElementById("fileInput");
+const progressArea = document.querySelector(".progress-area");
+const uploadedArea = document.querySelector(".uploaded-area");
 
-form.addEventListener("click", () => {
-  fileInput.click();
-});
-
-fileInput.onchange = ({ target }) => {
-  let file = target.files[0];
+// Listen for file selection
+fileInput.addEventListener("change", () => {
+  let file = fileInput.files[0];
   if (file) {
     let fileName = file.name;
     if (fileName.length >= 12) {
       let splitName = fileName.split(".");
       fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
     }
-    uploadFile(fileName);
+    uploadFile(fileName, file);
   }
-};
+});
 
-function uploadFile(name) {
+// Upload file function
+function uploadFile(name, file) {
   let xhr = new XMLHttpRequest();
-  xhr.open("POST", "upload.php");
+  xhr.open("POST", "https://prod-92.westus.logic.azure.com:443/workflows/b8aed472476d4e30a762027d45f29a3f/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=RvJN_krp58oEEFEz6KX-ye_0Yu-9v3hGlbN9JMQuMC4");
   xhr.upload.addEventListener("progress", ({ loaded, total }) => {
     let fileLoaded = Math.floor((loaded / total) * 100);
     let fileTotal = Math.floor(total / 1000);
@@ -59,17 +57,7 @@ function uploadFile(name) {
       uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
     }
   });
-  let data = new FormData(form);
-  xhr.send(data);
-}
-
-function confirmUpload() {
-  if (
-    confirm("Are you sure you want to submit the information for extraction?")
-  ) {
-    // uploadFile();
-    window.location.href = "loadin.html"; // Redirect to Validation.html
-  } else {
-    // User clicked Cancel, do nothing
-  }
+  let formData = new FormData();
+  formData.append("file", file);
+  xhr.send(formData);
 }
